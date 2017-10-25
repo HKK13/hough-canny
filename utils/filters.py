69926562b2653c279a90d7d7bd6d5953ab7df051
HKK13 <Kaan_K193@hotmail.com>
2017-10-25 18:08:53 +0300

    :sparkles: Add convolution

--- utils/filters.py
+++ utils/filters.py
@@ -1,15 +1,14 @@
 import numpy as np
-from scipy import ndimage
 
 
 def addPadding(image, padding):
     return np.pad(image, padding, 'constant', constant_values=0)
 
 
-"""
-Creates a custom sized 2D gaussian kernel.
-"""
 def create_gaussian_kernel(length, sigma=3):
+    """
+    Creates a custom sized 2D gaussian kernel.
+    """
     kernel = np.zeros((length, length))
 
     for y in range(length):
@@ -23,33 +22,27 @@ def create_gaussian_kernel(length, sigma=3):
     return kernel/np.sum(kernel)
 
 
+def convolve(image, kernel):
+    """
+    Takes an image and a kernel and returns the convolution of them
+    """
+    kernel = np.flipud(np.fliplr(kernel))
+    output = np.zeros_like(image)           # convolution output
+
+    kernel_i, kernel_j = kernel.shape
+    padding = kernel_i // 2
+
+    # Add zero padding to the input image
+    image_padded = np.zeros((image.shape[0] + 2 * padding, image.shape[1] + 2 * padding))
+    image_padded[padding:-padding, padding:-padding] = image
+    for x in range(image.shape[1]):         # Loop over every pixel of the image
+        for y in range(image.shape[0]):
+            # element-wise multiplication of the kernel and the image
+            output[y, x] = (kernel * image_padded[y:y + kernel_i, x:x + kernel_j]).sum()
+    return output
+
+
 def gaussian_filter(image, kernel_size, sigma=3):
     kernel = create_gaussian_kernel(kernel_size, sigma)
-    i_height, i_width = image.shape
-    k_height, k_width = kernel.shape
-
-    filtered = np.zeros_like(image)
-
-    for y in range(i_height):
-        for x in range(i_width):
-            weighted_pixel_sum = 0
-
-            for ky in range(-(k_height // 2), k_height//2):
-                for kx in range(-(k_width // 2), k_width//2):
-                    pixel = 0
-                    pixel_y = y - ky
-                    pixel_x = x - kx
-
-                    if (pixel_y >= 0) and (pixel_y < i_height) and (pixel_x >= 0) and (pixel_x < i_width):
-                        pixel = image[pixel_y][pixel_x]
-
-                    # get the weight at the current kernel position
-                    weight = kernel[ky + (k_height // 2)][kx + (k_width // 2)]
-
-                    # weigh the pixel value and sum
-                    weighted_pixel_sum += pixel * weight
-
-            # finally, the pixel at location (x,y) is the sum of the weighed neighborhood
-            filtered[y][x] = weighted_pixel_sum
-    return filtered
+    return convolve(image, kernel)
 
