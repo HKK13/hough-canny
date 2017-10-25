9d25d1888d7cf88cc08dd77510ece3d520561134
HKK13 <Kaan_K193@hotmail.com>
2017-10-26 01:02:04 +0300

    :wrench: Use only hysteresis thresold in canny

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from utils.io import rgb2gray, readImage
 from matplotlib import pyplot
-from utils.filters import \
-    addPadding, create_gaussian_kernel, gaussian_function
+from edge.canny import canny_edge_detection
+from PIL import Image
 
 
 def main():
-    kernel = create_gaussian_kernel(5, 1.5)
-    print(kernel)
+    image = readImage('im02.jpg')
+    img = rgb2gray(image)
+    edge = canny_edge_detection(img, 5, 3, 90, 150)
+
+    img_show = Image.fromarray(edge)
+    img_show = img_show.convert("L")
+    img_show.save('canny_edge.png')
+
 
 if __name__ == '__main__':
     main()
